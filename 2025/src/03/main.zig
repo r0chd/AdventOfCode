05cfe530656e4c742b234b292cfa121cc3cc4213
Oskar Rochowiak <os1@qed.ai>
2025-12-03 11:08:19 +0100

    03_1

--- 2025/src/03/main.zig
+++ 2025/src/03/main.zig
@@ -3,25 +3,42 @@ const zbench = @import("zbench");
 const mem = std.mem;
 const fs = std.fs;
 const heap = std.heap;
+const fmt = std.fmt;
 
-pub fn first(input: []const u8) !u32 {
-    _ = input;
-    return 0;
+pub fn first(comptime input: []const u8) !u32 {
+    var sum: u32 = 0;
+
+    var lines = mem.splitScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        var bytes = [2]u8{ 0, 0 };
+        for (line, 0..) |battery, i| {
+            if (battery > bytes[0] and line.len != i + 1) {
+                bytes[0] = battery;
+                bytes[1] = 0;
+            } else if (battery > bytes[1]) {
+                bytes[1] = battery;
+            }
+        }
+
+        sum += try std.fmt.parseInt(u32, bytes[0..2], 10);
+    }
+
+    return sum;
 }
 
-pub fn second(input: []const u8) !u32 {
+pub fn second(comptime input: []const u8) !u32 {
     _ = input;
     return 0;
 }
 
 fn benchFirst(alloc: mem.Allocator) void {
-    const input = std.mem.trimEnd(u8, @embedFile("./input.txt"), "\n");
+    const input = comptime std.mem.trimEnd(u8, @embedFile("./input.txt"), "\n");
     _ = first(input) catch {};
     _ = alloc;
 }
 
 fn benchSecond(alloc: mem.Allocator) void {
-    const input = std.mem.trimEnd(u8, @embedFile("./input.txt"), "\n");
+    const input = comptime std.mem.trimEnd(u8, @embedFile("./input.txt"), "\n");
     _ = second(input) catch {};
     _ = alloc;
 }
@@ -29,7 +46,7 @@ fn benchSecond(alloc: mem.Allocator) void {
 pub fn main() !void {
     const alloc = heap.page_allocator;
 
-    const input = mem.trimEnd(u8, @embedFile("./input.txt"), "\n");
+    const input = comptime mem.trimEnd(u8, @embedFile("./input.txt"), "\n");
     std.debug.print("First solution: {}\n", .{try first(input)});
     std.debug.print("Second solution: {}\n", .{try second(input)});
 
